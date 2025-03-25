83dfc3dc16c109acf88f9b44fb1b3667b933dc8c
Dustin Smith <dustinsmithdevelopment@gmail.com>
2025-03-25 08:55:26 -0500

    Adding restricted motion

---
+++ RestrictedRotation.ts
@@ -0,0 +1,11 @@
+import {Component} from "horizon/core";
+
+
+class RestrictedRotation extends Component<typeof RestrictedRotation> {
+  static propsDefinition = {};
+
+  start() {
+
+  }
+}
+Component.register(RestrictedRotation);
